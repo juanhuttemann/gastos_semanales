a5539e6b53236707a0c682918b969242ea13fe9e
Juan Huttemann <juanfhuttemann@gmail.com>
2018-10-28 21:56:06 -0300

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Form from './Form';
 
 class App extends Component {
+  state = {
+      presupuesto: '',
+      restante: '',
+      gastos: {}
+    }
+
+  agregarGasto = gasto => {
+    const gastos = {...this.state.gastos}
+
+    gastos[`gastos${Date.now()}`] = gasto
+
+    this.setState({
+      gastos
+    })
+
+    
+    
+  }
+
   render() {
     return (
       <div className="App">
         <Header titulo="Gastos Semanales"/>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+                <Form
+                  agregarGasto = {this.agregarGasto}
+                />
+            </div>
+            <div className="col-md-6">
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    nombreGasto     = React.createRef();
+    cantidadGasto   = React.createRef();
+
+    crearGasto = (e) => {
+        e.preventDefault();
+
+        const gasto = {
+            nombreGasto: this.nombreGasto.current.value,
+            cantidadGasto: this.cantidadGasto.current.value,
+        }
+
+
+        this.props.agregarGasto(gasto);
+        
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.crearGasto}>
+                <h2>Agrega tus gastos aquí</h2>
+                <div className="form-group">
+                    <label htmlFor="nombreGasto">Nombre Gasto:</label>
+                    <input ref={this.nombreGasto} type="text" className="form-control" id="nombreGasto" placeholder="Ej. Transporte"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="cantidadGasto">Cantidad:</label>
+                    <input ref={this.cantidadGasto} type="text" className="form-control" id="cantidadGasto" placeholder="Ej. 40" />
+                </div>
+                <button type="submit" className="btn btn-primary">Agregar</button>
+            </form>
+        )
+    }
+}
+
+export default Form;
